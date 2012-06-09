e056ada5100961e7861bd6ba338d847080d1e6c4
Julian Zell <devnull@localhost>
2012-06-10 01:54:22 +0200

    fixed an error in the genericprocessor which had the component be a package instead of a class

--- src/de/unihd/dbs/uima/annotator/heideltime/processors/GenericProcessor.java
+++ src/de/unihd/dbs/uima/annotator/heideltime/processors/GenericProcessor.java
@@ -11,14 +11,14 @@ import org.apache.uima.jcas.JCas;
  *
  */
 public abstract class GenericProcessor {
-	protected Package component;
+	private Class<?> component;
 	
 	/**
 	 * Constructor that sets the component for logger use.
 	 * Any inheriting class should run this via super()
 	 */
 	public GenericProcessor() {
-		this.component = this.getClass().getPackage();
+		this.component = this.getClass();
 	}
 	
 	/**
