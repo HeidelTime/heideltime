8da0ec61f5afe07896972bf134cde3bdb70b35f3
Julian Zell <devnull@localhost>
2012-06-11 01:02:26 +0200

    altered heideltimeexception to optionally contain an error message

--- src/de/unihd/dbs/uima/annotator/heideltime/HeidelTimeException.java
+++ src/de/unihd/dbs/uima/annotator/heideltime/HeidelTimeException.java
@@ -1,5 +1,5 @@
 /**
- * 
+ * A dedicated Exception type for HeidelTime-based Exceptions.
  */
 package de.unihd.dbs.uima.annotator.heideltime;
 
@@ -12,5 +12,16 @@ public class HeidelTimeException extends Exception {
 	 * automatically generated uid
 	 */
 	private static final long serialVersionUID = 5934916764509083459L;
+	private String message;
+	
+	public HeidelTimeException() {};
+	
+	public HeidelTimeException(String message) {
+		this.message = message;
+	}
+	
+	public String getMessage() {
+		return this.message;
+	}
 
 }
