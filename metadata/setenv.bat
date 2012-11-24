b9ccd0fa47d5070a69a13d5be9b7bedcb6113636
Julian Zell <devnull@localhost>
2012-11-24 15:25:41 +0100

    added a setenv script for windows environments

---
+++ metadata/setenv.bat
@@ -0,0 +1,6 @@
+REM setting the environment for the HeidelTime kit
+set CLASSPATH=%HEIDELTIME_HOME%/class;%CLASSPATH%
+set CLASSPATH=%HEIDELTIME_HOME%/lib;%CLASSPATH%
+REM setting the environment for DKPRO (required by HeidelTime)
+set CLASSPATH=%DKPRO_HOME%/bin;%CLASSPATH%
+set CLASSPATH=%DKPRO_HOME%/lib;%CLASSPATH%
