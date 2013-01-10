2a111e4a6ef5f7f968f291c46f8dce3b30b70ea6
Julian Zell <devnull@localhost>
2013-01-10 14:22:11 +0100

    Starting 'arabic-resources' branch

--- src/de/unihd/dbs/uima/annotator/heideltime/resources/Language.java
+++ src/de/unihd/dbs/uima/annotator/heideltime/resources/Language.java
@@ -16,8 +16,9 @@ public enum Language {
 	DUTCH		("dutch", "dutch", "dutch", ""),
 	ENGLISHCOLL	("englishcoll", "englishcoll", "english", "-e"),
 	ENGLISHSCI	("englishsci", "englishsci", "english", "-e"),
-	//ITALIAN		("italian", "italian", "italian", "-i"),
-	//FRENCH		("french", "french", "french", "-f"),
+	ITALIAN		("italian", "italian", "italian", "-i"),
+	FRENCH		("french", "french", "french", "-f"),
+	ARABIC		("arabic", "arabic", "arabic", ""),
 	WILDCARD	("", "", "", ""), // if no match was found, this gets filled with parameter
 	; // ends the enum element list
 	
