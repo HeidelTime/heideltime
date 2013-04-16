4bb27b878559bd376e2ad680134dc7570fbe9725
Julian Zell <j.zell@stud.uni-heidelberg.de>
2013-04-16 02:19:47 +0200

    added stanfordtagger/jvntextpro to windows setenv script

--- metadata/setenv.bat
+++ metadata/setenv.bat
@@ -5,3 +5,7 @@ set CLASSPATH=%HEIDELTIME_HOME%\lib;%CLASSPATH%
 REM setting the environment for DKPRO (required by HeidelTime)
 set CLASSPATH=%DKPRO_HOME%\bin;%CLASSPATH%
 set CLASSPATH=%DKPRO_HOME%\lib;%CLASSPATH%
+REM setting the environment for jvntextpro (required for the wrapper)
+set CLASSPATH=%JVNTEXTPRO_HOME%;%CLASSPATH%
+REM setting the environment for the stanford pos tagger (required for the wrapper)
+set CLASSPATH=%STANFORDTAGGER%;%CLASSPATH%
