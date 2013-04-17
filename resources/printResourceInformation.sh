5fed6e0daf1c004cffa703468e03bdfa7e900148
Julian Zell <j.zell@stud.uni-heidelberg.de>
2013-04-18 00:48:16 +0200

    improved printResourceInformation.sh script to not copy data into folders relative to the working directory

--- resources/printResourceInformation.sh
+++ resources/printResourceInformation.sh
@@ -1,9 +1,18 @@
 #!/bin/bash
 
+if [ "$HEIDELTIME_HOME" = "" ] ; then
+	echo "please set \$HEIDELTIME_HOME"
+	exit -1
+fi
+
+PWD=$(pwd)
+cd $HEIDELTIME_HOME/resources
+
 echo "Writing used_resources.txt"
 find ./ -name "*.txt"  > used_resources.txt
 
 echo "Copying resources..."
-cp -r * ../class/ 
+cp -r $HEIDELTIME_HOME/resources/* $HEIDELTIME_HOME/class/ 
 
 echo "done."
+cd $PWD
