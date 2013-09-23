695efb38743d42c2f3b2b0cf88dada3090a2204e
Julian Zell <zell@informatik.uni-heidelberg.de>
2013-09-23 14:31:45 +0200

    removed an unused import

--- src/de/unihd/dbs/uima/reader/tempeval2reader/Tempeval2Reader.java
+++ src/de/unihd/dbs/uima/reader/tempeval2reader/Tempeval2Reader.java
@@ -17,7 +17,6 @@ package de.unihd.dbs.uima.reader.tempeval2reader;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileInputStream;
-import java.io.FileReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.Charset;
