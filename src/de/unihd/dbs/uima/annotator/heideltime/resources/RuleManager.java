de8fdb1df7e44a9775607c6174da7be905940c42
Julian Zell <zell@informatik.uni-heidelberg.de>
2015-04-16 20:39:52 +0200

    cleanup of imports

--- src/de/unihd/dbs/uima/annotator/heideltime/resources/RuleManager.java
+++ src/de/unihd/dbs/uima/annotator/heideltime/resources/RuleManager.java
@@ -6,11 +6,8 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.Collections;
 import java.util.Comparator;
-import java.util.EnumMap;
 import java.util.HashMap;
 import java.util.LinkedList;
-import java.util.Set;
-import java.util.TreeSet;
 import java.util.regex.MatchResult;
 import java.util.regex.Pattern;
 
