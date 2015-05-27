8c445d82dc66203432f12ab4551871b57f4b3dd7
Julian Zell <zell@informatik.uni-heidelberg.de>
2015-05-27 13:49:01 +0200

    minor fix for hunpos annotation translator to also find its configuration file in a UIMA kit.

--- src/hr/fer/zemris/takelab/uima/annotator/hunpos/HunPosAnnotionTranslator.java
+++ src/hr/fer/zemris/takelab/uima/annotator/hunpos/HunPosAnnotionTranslator.java
@@ -1,8 +1,11 @@
 package hr.fer.zemris.takelab.uima.annotator.hunpos;
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
@@ -22,8 +25,17 @@ public class HunPosAnnotionTranslator {
 	
 	private void loadTranslations() {
 		BufferedReader reader = null;
+		InputStream is = null;
 		try {
-			reader = new BufferedReader(new InputStreamReader(getClass().getClassLoader().getResourceAsStream("croatian/TagTranslation.conf")));
+			File jarFile = new File(this.getClass().getProtectionDomain().getCodeSource().getLocation().getPath());
+			if(jarFile.isFile()) {
+				is = getClass().getClassLoader().getResourceAsStream("croatian/TagTranslation.conf"); 
+			} else {
+				File tagFile = new File(new File(jarFile, ".."), "resources/croatian/TagTranslation.conf");
+				is = new FileInputStream(tagFile);
+			}
+			
+			reader = new BufferedReader(new InputStreamReader(is));
 			Pattern reRule = Pattern.compile("^\\s*\"([^\"]+)\"\\s*=\\s*\"([^\"]+)\"\\s*$");
 			String line;
 			while((line = reader.readLine()) != null) {
@@ -48,7 +60,9 @@ public class HunPosAnnotionTranslator {
 			Logger.printError("Error reading HunPos annotation translation rules file.");
 		} finally {
 			try {
-				reader.close();
+				if(reader != null) {
+					reader.close();
+				}
 			} catch (IOException e) {
 				Logger.printError("An error occured while closing the file.");
 			}
