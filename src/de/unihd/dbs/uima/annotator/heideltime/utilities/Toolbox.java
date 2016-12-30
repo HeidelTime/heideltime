2dd7f237d552ebb3b0f6f7c46f3fe839d8020128
Erich Schubert <schubert@informatik.uni-heidelberg.de>
2016-12-30 14:09:14 +0100

    More optimization.

--- src/de/unihd/dbs/uima/annotator/heideltime/utilities/Toolbox.java
+++ src/de/unihd/dbs/uima/annotator/heideltime/utilities/Toolbox.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.regex.MatchResult;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
@@ -42,22 +43,12 @@ public class Toolbox {
 	 * @param m Map of items to sort
 	 * @return sorted List of items
 	 */
-    public static List<Pattern> sortByValue(final HashMap<Pattern,String> m) {
-        List<Pattern> keys = new ArrayList<Pattern>();
-        keys.addAll(m.keySet());
-        Collections.sort(keys, new Comparator<Object>() {
-            @SuppressWarnings({ "unchecked", "rawtypes" })
-			public int compare(Object o1, Object o2) {
-                Object v1 = m.get(o1);
-                Object v2 = m.get(o2);
-                if (v1 == null) {
-                    return (v2 == null) ? 0 : 1;
-                } else if (v1 instanceof Comparable) {
-                    return ((Comparable) v1).compareTo(v2);
-                } else {
-                    return 0;
-                }
-            }
+    public static List<Map.Entry<Pattern, String>> sortByValue(final HashMap<Pattern,String> m) {
+        List<Map.Entry<Pattern, String>> keys = new ArrayList<>(m.entrySet());
+        Collections.sort(keys, new Comparator<Map.Entry<Pattern, String>>() {
+        	public int compare(Map.Entry<Pattern, String> o1, Map.Entry<Pattern, String> o2) {
+        		return o1.getValue().compareTo(o2.getValue());
+        	}
         });
         return keys;
     }
